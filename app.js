76ef1be6572c638adee8a0df326d7bac8a2da693
David <dmartin5150@icloud.com>
2022-04-23 09:41:55 -0500

    base CSS start

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app = express();
 app.set('view engine','ejs'); //use ejs 
 app.set('views',path.join(__dirname,'views')); //where to find views
 
+app.use(express.static('public'));
+
 app.use(authRoutes);
 
 app.listen(3000);
