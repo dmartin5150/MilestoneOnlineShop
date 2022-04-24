71ea1477db10368b42912364a8ae86b9794161c5
David <dmartin5150@icloud.com>
2022-04-24 11:08:06 -0500

    added connection to mongodb

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,11 +3,16 @@ function getSignUp(req, res) {
   res.render('customer/auth/signup');
 }
 
+function signup(req, res) {
+
+}
+
 function getLogin(req, res) {
 
 }
 
 module.exports = {
   getSignUp : getSignUp,
-  getLogin: getLogin
+  getLogin: getLogin,
+  signup: signup
 };
