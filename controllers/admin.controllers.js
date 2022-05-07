78ea2af2a0c9b565c2daf841d39885f60c83fb7d
David <dmartin5150@icloud.com>
2022-05-06 21:44:19 -0500

    ajax orders

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -1,54 +1,49 @@
-const removeUploadedFiles = require('multer/lib/remove-uploaded-files');
 const Product = require('../models/product.model');
+const Order = require('../models/order.model');
 
-
-async function getProducts (req,res, next) {
+async function getProducts(req, res, next) {
   try {
     const products = await Product.findAll();
-    res.render('admin/products/all-products', {products: products});
-  } catch(error) {
+    res.render('admin/products/all-products', { products: products });
+  } catch (error) {
     next(error);
     return;
   }
-
 }
 
-function getNewProduct(req,res) {
+function getNewProduct(req, res) {
   res.render('admin/products/new-product');
 }
 
-async function createNewProduct(req, res,next) {
-
+async function createNewProduct(req, res, next) {
   const product = new Product({
     ...req.body,
-    image: req.file.filename
+    image: req.file.filename,
   });
 
   try {
     await product.save();
-  }
-  catch (error) {
+  } catch (error) {
     next(error);
     return;
   }
+
   res.redirect('/admin/products');
 }
 
-
 async function getUpdateProduct(req, res, next) {
   try {
-  const product = await Product.findById(req.params.id);
-  res.render('admin/products/update-product',{product:product})
-  } catch(error) {
+    const product = await Product.findById(req.params.id);
+    res.render('admin/products/update-product', { product: product });
+  } catch (error) {
     next(error);
   }
 }
 
-
-async function updateProduct(req, res,next) {
+async function updateProduct(req, res, next) {
   const product = new Product({
     ...req.body,
-    _id: req.params.id
+    _id: req.params.id,
   });
 
   if (req.file) {
@@ -56,13 +51,13 @@ async function updateProduct(req, res,next) {
   }
 
   try {
-    await product.save()
-  } catch (error){
+    await product.save();
+  } catch (error) {
     next(error);
     return;
   }
-  res.redirect('/admin/products');
 
+  res.redirect('/admin/products');
 }
 
 async function deleteProduct(req, res, next) {
@@ -70,12 +65,39 @@ async function deleteProduct(req, res, next) {
   try {
     product = await Product.findById(req.params.id);
     await product.remove();
-  } catch(error) {
+  } catch (error) {
     return next(error);
   }
 
-  res.json({message: 'Deleted Product'});
+  res.json({ message: 'Deleted product!' });
+}
 
+async function getOrders(req, res, next) {
+  try {
+    const orders = await Order.findAll();
+    res.render('admin/orders/admin-orders', {
+      orders: orders
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateOrder(req, res, next) {
+  const orderId = req.params.id;
+  const newStatus = req.body.newStatus;
+
+  try {
+    const order = await Order.findById(orderId);
+
+    order.status = newStatus;
+
+    await order.save();
+
+    res.json({ message: 'Order updated', newStatus: newStatus });
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
@@ -84,5 +106,7 @@ module.exports = {
   createNewProduct: createNewProduct,
   getUpdateProduct: getUpdateProduct,
   updateProduct: updateProduct,
-  deleteProduct: deleteProduct
-}
+  deleteProduct: deleteProduct,
+  getOrders: getOrders,
+  updateOrder: updateOrder
+};
